e7861b50da0297ffc804909de960534dd030de10
OriginarioG <silvakleberson740@gmail.com>
2025-12-25 16:56:14 -0300

    reposter.js

--- reposter.js
+++ reposter.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("<SECRET_BOT_TOKEN>").catch(console.error);
+client.login("1453819992389845105").catch(console.error);
 
 client.on("ready", function() {
 	const serverCount = client.guilds.cache.size;
